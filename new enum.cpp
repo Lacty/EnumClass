edb6e0edd526fe9be196c6d4ce19e75ef79fbfb3
U-PS14B22\vantan <akira206@gmail.com>
2015-04-04 13:04:47 +0900

    new enum

---
+++ new enum.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+// enum(—ñ‹“‘Ì)
+
+
+// C++11
+enum class Team {
+  RED,
+  BLUE
+};
+
+// Œ^Žw’è
+enum class Button : char {
+  UP    = 'W',
+  LEFT  = 'A',
+  DOWN  = 'S',
+  RIGHT = 'D'
+};
+
+
+int main(){
+  Button button = Button::UP;
+
+}
